966d351a1d923606437d8b5aad6f33e755c50507
vinay nandyala <>
2020-10-04 18:01:06 +0530

    donwloading images on page end scroll

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,10 +14,11 @@ import {
   Text,
   StatusBar,
   FlatList,
-} from 'react-native';
-import SiteComponent from './Site';
-import Axios from 'axios';
-
+  Dimensions,
+} from "react-native";
+import SiteComponent from "./Site";
+import Axios from "axios";
+const { height } = Dimensions.get("window");
 class App extends React.Component {
   constructor() {
     super();
@@ -27,24 +28,25 @@ class App extends React.Component {
   }
 
   async componentDidMount() {
-    this.fetchResult();
+    this.fetchResult(0, 5);
   }
   renderImageView = (item) => {
-    console.log('FlatList: item', item.urls.small);
+    console.log("FlatList: item", item.urls.small);
     return (
       <SiteComponent logo={item.urls.small} name="this is sample download" />
     );
   };
-  fetchModeDateFromAPI = () => {
-    const photoUrl = `https://api.unsplash.com/search/photos?client_id=MgllgRq7xd3TYgttzB1esqxfnFvC90sn9HLbUTRWclw&query=canada&page=1&per_page=5`;
+  fetchModeDateFromAPI = (limit) => {
+    const photoUrl = `https://api.unsplash.com/search/photos?client_id=MgllgRq7xd3TYgttzB1esqxfnFvC90sn9HLbUTRWclw&query=canada&per_page=${limit}`;
     return Axios.get(photoUrl);
   };
 
-  fetchResult = () => {
-    const {offset, limit} = this.state;
-    this.fetchModeDateFromAPI(offset, limit)
+  fetchResult = (distanceFromEnd, limit) => {
+    const { offset } = this.state;
+    console.log("coming to end of the page", distanceFromEnd, limit);
+    this.fetchModeDateFromAPI(limit)
       .then((res) => {
-        console.log('coming to fetchResult', res.data.results);
+        console.log("coming to fetchResult", res.data.results);
 
         if (!res.data.results) {
           return;
@@ -56,22 +58,28 @@ class App extends React.Component {
         });
       })
       .catch((error) => {
-        console.log('fetchModeDateFromAPI:Error', error);
+        console.log("fetchModeDateFromAPI:Error", error);
       });
   };
 
   loadInfiteImages = () => {
-    console.log('loadInfiteImages');
-    const {list} = this.state;
+    console.log("loadInfiteImages");
+    const { list, limit } = this.state;
     if (list && list.length > 0) {
       return (
         <FlatList
-          style={{flex: 1}}
+          style={{ flex: 1 }}
           extraData={this.state}
-          onEndReached={this.fetchResult}
-          onEndReachedThreshold={0.7}
+          onEndReached={({ distanceFromEnd }) => {
+            console.log("distanceFromEnd", distanceFromEnd);
+            if (distanceFromEnd < 0) {
+              return;
+            }
+            this.fetchResult(distanceFromEnd, limit + 5);
+          }}
+          onEndReachedThreshold={0.2}
           data={this.state.list}
-          renderItem={({item}) => this.renderImageView(item)}
+          renderItem={({ item }) => this.renderImageView(item)}
           keyExtractor={(item) => item.id.toString()}
         />
       );
@@ -89,6 +97,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginBottom: 10,
+    height: height,
   },
 });
 
